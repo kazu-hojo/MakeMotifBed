1c590d319bc7e3ec1f9ecc1f6db6df28be8db976
kazu-hojo <hojo1411@g.ecc.u-tokyo.ac.jp>
2023-01-13 21:58:03 +0900

    Second commit

---
+++ R/MakeMotifBed.R
@@ -0,0 +1,26 @@
+#' @title Create bed file of motif you are interested in
+#' @description /code{mk.motif.bed} create bed file of motif you are interested in
+
+#' @param Motif Motif you are interested in
+#' @param Genome Input fasta file of genome you are using
+#' @return Dataframe of input motif in bed file format
+#' @export
+#' @example
+#' # mk.motif.bed("Motif")
+
+library("Biostrings")
+library("dplyr")
+
+mk.motif.bed <- function(Motif, Genome){
+  Genome <- readDNAStringSet(Genome, format = "fasta")
+  motif <- vmatchPattern(Motif, Genome)
+  n <- 3
+  motif.all.chr <- data.frame(matrix(rep(NA, n), nrow=1))[numeric(0), ]
+  colnames(motif.all.chr) <- c("chr", "start", "end")
+  for (i in 1:length(motif)) {
+    assign(paste(Motif, motif@NAMES[i], sep = "."), data.frame(chr = rep(motif@NAMES[i], length(motif@ends[i])), start = motif@ends[[i]], end = motif@ends[[i]]))
+    motif.all.chr <- rbind(motif.all.chr, data.frame(eval(parse(text = paste(Motif, motif@NAMES[i], sep = ".")))))
+  }
+  motif.all.chr$start <- motif.all.chr$start - (nchar(Motif) - 1)
+  return(motif.all.chr)
+}
